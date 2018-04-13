7298e278e9e36b8b7704e6904b83fb1c7b0253e6
Patrick DePuydt <prdepuydt@gmail.com>
2018-04-13 08:25:15 -0400

    Style form

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import {ChineseHeader} from "../../assets/shared/js/chineseHeader";
-import { SharedBanner } from '../../assets/shared/js/sharedBanner';
-import { SharedSection } from '../../assets/shared/js/sharedSection';
-import { SharedParagraph } from '../../assets/shared/js/paragraph';
-import ContactForm from './contactForm';
-
+import { ChineseHeader } from '../../assets/shared/js/chineseHeader'
+import { SharedBanner } from '../../assets/shared/js/sharedBanner'
+import { SharedSection } from '../../assets/shared/js/sharedSection'
+import { SharedParagraph } from '../../assets/shared/js/paragraph'
+import ContactForm from './contactForm'
+import { Black, Cream } from '../../assets/shared/js/swatches';
 
 const StyledHeadingsContainer = styled.div`
   padding: 0 1em;
+`
+
+const LocalStyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+
+const LocalStyledInput = styled.input`
+  margin: 20px 0;
+  border: 4px solid ${Black};
+  padding: 20px;
+  color: ${Black};
+  font-family: 'Source Sans Pro';
+  font-size: 18px;
+  outline: none;
+`
+
+const LocalStyledTextArea = styled.textarea`
+  margin: 20px 0;
+  border: 4px solid ${Black};
+  padding: 20px;
+  color: ${Black};
+  font-family: 'Source Sans Pro';
+  font-size: 18px;
+  outline: none;
+`
+const LocalStyledButton = styled.button`
+  margin: 20px 0;
+  background: ${Black};
+  color: ${Cream};
+  padding: 20px;
+  font-family: 'Playfair Display';
+  font-size: 30px;
 `
 
 const Contact = () => (
@@ -25,20 +59,20 @@ const Contact = () => (
       dreamcatcher tbh listicle.
     </SharedParagraph>
 
-    <form
+    <LocalStyledForm
       name="contact-form"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <input name="name" placeholder="Your Name" type="text" />
-      <input name="email" placeholder="name@name.com" type="email" />
-      <textarea name="message" />
-      <button>Send</button>
-    </form>
+      <LocalStyledInput name="name" placeholder="Your Name" type="text" />
+      <LocalStyledInput name="email" placeholder="name@name.com" type="email" />
+      <LocalStyledTextArea name="message" />
+      <LocalStyledButton>Send</LocalStyledButton>
+    </LocalStyledForm>
 
     <ContactForm />
   </SharedSection>
 )
 
-export default Contact;
+export default Contact
