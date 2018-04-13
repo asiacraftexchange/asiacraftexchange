169a11f3aeafe84584910424a10224092eee1f1b
Patrick DePuydt <prdepuydt@gmail.com>
2018-04-13 07:59:59 -0400

    Add contact form

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -18,14 +18,26 @@ const Contact = () => (
       <ChineseHeader>联系我们</ChineseHeader>
       <SharedBanner>Contact Us</SharedBanner>
     </StyledHeadingsContainer>
-     
-      <SharedParagraph>
-        Lorem ipsum dolor amet tousled unicorn mustache, you probably haven't
-        heard of them prism meh kombucha leggings locavore. Truffaut four loko
-        dreamcatcher tbh listicle.
-      </SharedParagraph>
 
-      <ContactForm />
+    <SharedParagraph>
+      Lorem ipsum dolor amet tousled unicorn mustache, you probably haven't
+      heard of them prism meh kombucha leggings locavore. Truffaut four loko
+      dreamcatcher tbh listicle.
+    </SharedParagraph>
+
+    <form
+      name="contact-form"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input name="name" placeholder="Your Name" type="text" />
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
+      <button>Send</button>
+    </form>
+
+    <ContactForm />
   </SharedSection>
 )
 
